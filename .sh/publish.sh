7a1bf494b0886b4c16b6f606b6cedab3033620fb
Justin ONeill <justinoneill2007@gmail.com>
2023-01-04 19:06:16 -0600

    updated

---
+++ .sh/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Bump npm version and create git tag with the new version
+npm version
+
+# Add all changes to git
+git add .
+
+# Commit the changes with the message "Updated version"
+git commit -m "Updated version"
+
+# Push the changes to the remote repository
+# git push
+
+# Change directory to package
+cd package
+
+# Publish the package to npm with public access
+# npm publish --access public
+
+# Change directory to root
+cd ..
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import './app.css';
+// import './app.css';
 
 /* COMPONENTS
 ▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃ */
