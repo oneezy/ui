36b053ef2a6e82c860831e5f1b2ac4c25bd3975b
Justin ONeill <justinoneill2007@gmail.com>
2023-01-26 15:36:10 -0600

    fix: removed describe

--- src/lib/components/organisms/Stats/Stats.svelte
+++ src/lib/components/organisms/Stats/Stats.svelte
@@ -1,56 +1,15 @@
 <script>
 	import { Content, CountUp, viewport } from '$ui'
 
-
-// export let stats = writable(
-// 	{ name: 'Users', value: '8956', type: 'decimal', duration: 1000 },
-// 	{ name: 'Bots', value: '2740', type: 'decimal', duration: 1000 },
-// 	{ name: 'Exchanges', value: '2', type: 'decimal', duration: 1000 },
-// 	{ name: 'Trading Pairs', value: '106', type: 'decimal', duration: 3000 }
-// )
-
-export let stats = [
-	{ name: 'Users', value: '8956', type: 'decimal', duration: 1000 },
-	{ name: 'Bots', value: '2740', type: 'decimal', duration: 1000 },
-	{ name: 'Exchanges', value: '2', type: 'decimal', duration: 1000 },
-	{ name: 'Trading Pairs', value: '106', type: 'decimal', duration: 3000 }
-]
-
-  /*
-  **********************************************************************************
-    We need to put populate the <Stats /> component depending on it's environment
-  **********************************************************************************
-
-	// import { env } from '$env/dynamic/public'
-	import { describe } from '$ui'
-  import { stats } from '../stores/stats'
-
-	async function fetchData() {
-		// const response = await fetch(env.PUBLIC_APIURL + '/describe')
-    const response = await fetch('https://api-dev.scavengerbot.io/v1/describe')
-		const data = await response.json()
-		describe.set(data.data)
-		// loading = false
-	}
-
-	// on mount update the describe store every 1 minute (60,000ms)
-	onMount(async () => {
-		await fetchData()
-
-		const interval = setInterval(async () => {
-			await fetchData()
-		}, 60000)
-	})
-
-	// on destroy, stop the interval
-	onDestroy(() => {
-		clearInterval(interval)
-	})
-*/
+  export let stats = [
+    { name: 'Users', value: '8956', type: 'decimal', duration: 1000 },
+    { name: 'Bots', value: '2740', type: 'decimal', duration: 1000 },
+    { name: 'Exchanges', value: '2', type: 'decimal', duration: 1000 },
+    { name: 'Trading Pairs', value: '106', type: 'decimal', duration: 3000 }
+  ]
 </script>
 
 <Content class="w-full flex items-center justify-center lg:mx-0 lg:w-auto lg:gap-20">
-	<!-- {#each $stats as stat} -->
 	{#each stats as stat}
 		<div class="w-1/2 lg:w-auto grid lg:gap-4">
 			<h4
