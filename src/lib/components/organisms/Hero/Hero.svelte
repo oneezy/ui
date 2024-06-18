c06086b36f1c14b079265102b423f63d39daf8a1
oneezy <justinoneill2007@gmail.com>
2024-06-18 17:20:44 -0500

    button color change

--- src/lib/components/organisms/Hero/Hero.svelte
+++ src/lib/components/organisms/Hero/Hero.svelte
@@ -52,7 +52,7 @@
 		{/if}
 
 		<div class="flex flex-col justify-center gap-4 lg:flex-row">
-			<Button class="min-w-96 flex-1" href={button1Link} xl primary><PhoneIcon class="text-4xl" />{button1}</Button
+			<Button class="min-w-96 flex-1" href={button1Link} xl secondary><PhoneIcon class="text-4xl" />{button1}</Button
 			>
 			<Button class="min-w-96 flex-1" href={button2Link} neutral xl
 				><MapIcon class="text-4xl" />{button2}</Button
